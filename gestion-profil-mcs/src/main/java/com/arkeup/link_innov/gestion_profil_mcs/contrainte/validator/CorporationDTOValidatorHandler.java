8601275212691f3ae1e3ff1c1c4083382bb4cc6e
(no author) <(no author)@dd330bbb-b17d-40ae-9b34-6f06ae2eda2d>
2020-04-21 11:41:36 +0000

    LIN-311
    git-svn-id: http://35.195.213.250/svn/com.arkeup.link_innov/server/GestionProfilMCS/branches/BP_NOTIF@10863 dd330bbb-b17d-40ae-9b34-6f06ae2eda2d

--- gestion-profil-mcs/src/main/java/com/arkeup/link_innov/gestion_profil_mcs/contrainte/validator/CorporationDTOValidatorHandler.java
+++ gestion-profil-mcs/src/main/java/com/arkeup/link_innov/gestion_profil_mcs/contrainte/validator/CorporationDTOValidatorHandler.java
@@ -30,12 +30,14 @@ public class CorporationDTOValidatorHandler {
 	public Errors validate(Object obj) {
 		Errors errors = new BindException(obj, "coporationDTO");
 		boolean isValid = supports(obj);
-		if(!isValid) {
+		if (!isValid) {
 			return errors;
 		}
 
-		
-		String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+//		String urlRegex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+		String urlRegex = "((http?|https|ftp|file)://)?((W|w){3}.)?[a-zA-Z0-9]+\\.[a-zA-Z]+";
+
 		Pattern pattern = Pattern.compile(urlRegex);
 
 		CorporationDTO corporationDTO = (CorporationDTO) obj;
@@ -56,7 +58,8 @@ public class CorporationDTOValidatorHandler {
 			}
 			if ((corporationDTO.getThematicArea() == null)
 					|| (StringUtils.isEmpty(corporationDTO.getThematicArea().getLabel()))) {
-				errors.rejectValue("thematicArea", "ERR_MCS_PROFIL_0029", ErrorsEnum.ERR_MCS_PROFIL_0029.getErrorMessage());
+				errors.rejectValue("thematicArea", "ERR_MCS_PROFIL_0029",
+						ErrorsEnum.ERR_MCS_PROFIL_0029.getErrorMessage());
 			}
 		}
 
@@ -72,7 +75,8 @@ public class CorporationDTOValidatorHandler {
 
 			if ((corporationDTO.getActivityArea() == null)
 					|| (StringUtils.isEmpty(corporationDTO.getActivityArea().getLabel()))) {
-				errors.rejectValue("activityArea", "ERR_MCS_PROFIL_0032", ErrorsEnum.ERR_MCS_PROFIL_0032.getErrorMessage());
+				errors.rejectValue("activityArea", "ERR_MCS_PROFIL_0032",
+						ErrorsEnum.ERR_MCS_PROFIL_0032.getErrorMessage());
 			}
 		}
 
@@ -83,7 +87,8 @@ public class CorporationDTOValidatorHandler {
 			}
 			if ((corporationDTO.getActivityArea() == null)
 					|| (StringUtils.isEmpty(corporationDTO.getActivityArea().getLabel()))) {
-				errors.rejectValue("activityArea", "ERR_MCS_PROFIL_0038", ErrorsEnum.ERR_MCS_PROFIL_0038.getErrorMessage());
+				errors.rejectValue("activityArea", "ERR_MCS_PROFIL_0038",
+						ErrorsEnum.ERR_MCS_PROFIL_0038.getErrorMessage());
 			}
 		}
 
@@ -105,7 +110,86 @@ public class CorporationDTOValidatorHandler {
 				&& !pattern.matcher(corporationDTO.getWebSite()).matches()) {
 			errors.rejectValue("webSite", "ERR_MCS_PROFIL_0037", ErrorsEnum.ERR_MCS_PROFIL_0037.getErrorMessage());
 		}
-		
+
 		return errors;
+	}
+
+	public CorporationDTO validateDTO(Object obj) {
+		boolean isValid = supports(obj);
+		if (!isValid) {
+			return new CorporationDTO();
+		}
+
+		String urlRegex = "((http?|https|ftp|file)://)?((W|w){3}.)?[a-zA-Z0-9]+\\.[a-zA-Z]+";
+
+		Pattern pattern = Pattern.compile(urlRegex);
+
+		CorporationDTO corporationDTO = (CorporationDTO) obj;
+		CategoryDTO type = corporationDTO.getType();
+
+		if (StringUtils.isEmpty(corporationDTO.getName())) {
+			corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0013.getErrorMessage());
+		}
+
+		if (type == null) {
+			corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0027.getErrorMessage());
+		}
+
+		if (type != null && StringUtils.equals("uuid-category-academique", type.getId())) {
+			if ((corporationDTO.getTypology() == null)
+					|| (StringUtils.isEmpty(corporationDTO.getTypology().getLabel()))) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0028.getErrorMessage());
+			}
+			if ((corporationDTO.getThematicArea() == null)
+					|| (StringUtils.isEmpty(corporationDTO.getThematicArea().getLabel()))) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0029.getErrorMessage());
+			}
+		}
+
+		if (type != null && StringUtils.equals("uuid-category-industriel", type.getId())) {
+			if (StringUtils.isEmpty(corporationDTO.getSiret())) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0030.getErrorMessage());
+			}
+			if ((corporationDTO.getEmployeesNumber() == null)
+					|| (StringUtils.isEmpty(corporationDTO.getEmployeesNumber().getLabel()))) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0031.getErrorMessage());
+			}
+
+			if ((corporationDTO.getActivityArea() == null)
+					|| (StringUtils.isEmpty(corporationDTO.getActivityArea().getLabel()))) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0032.getErrorMessage());
+			}
+		}
+
+		if (type != null && StringUtils.equals("uuid-category-autre-acteur", type.getId())) {
+			if (StringUtils.isEmpty(corporationDTO.getClassification().getLabel())) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0033.getErrorMessage());
+			}
+			if ((corporationDTO.getActivityArea() == null)
+					|| (StringUtils.isEmpty(corporationDTO.getActivityArea().getLabel()))) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0038.getErrorMessage());
+			}
+		}
+
+		for (KeywordDTO keyword : corporationDTO.getKeywords()) {
+			if (!StringUtils.isEmpty(keyword.getLabel()) && keyword.getLabel().length() > 50) {
+				corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0034.getErrorMessage());
+			}
+		}
+
+		if (!StringUtils.isEmpty(corporationDTO.getDescription()) && corporationDTO.getDescription().length() > 2000) {
+			corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0036.getErrorMessage());
+		}
+
+		if (!StringUtils.isEmpty(corporationDTO.getSiret()) && corporationDTO.getSiret().length() > 14) {
+			corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0035.getErrorMessage());
+		}
+
+		if (StringUtils.isNotEmpty(corporationDTO.getWebSite())
+				&& !pattern.matcher(corporationDTO.getWebSite()).matches()) {
+			corporationDTO.setErrorCode(ErrorsEnum.ERR_MCS_PROFIL_0037.getErrorMessage());
+		}
+
+		return corporationDTO;
 	}
 }
