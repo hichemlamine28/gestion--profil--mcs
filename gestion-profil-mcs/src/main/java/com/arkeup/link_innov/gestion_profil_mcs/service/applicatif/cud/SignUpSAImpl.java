360d09c2f47b2c775d02b0f493bd8d049f010eef
(no author) <(no author)@dd330bbb-b17d-40ae-9b34-6f06ae2eda2d>
2020-07-07 16:27:57 +0000

    LIN-440
    git-svn-id: http://35.195.213.250/svn/com.arkeup.link_innov/server/GestionProfilMCS/branches/BP_NOTIF@11384 dd330bbb-b17d-40ae-9b34-6f06ae2eda2d

--- gestion-profil-mcs/src/main/java/com/arkeup/link_innov/gestion_profil_mcs/service/applicatif/cud/SignUpSAImpl.java
+++ gestion-profil-mcs/src/main/java/com/arkeup/link_innov/gestion_profil_mcs/service/applicatif/cud/SignUpSAImpl.java
@@ -676,11 +676,11 @@ public class SignUpSAImpl implements SignUpSA {
 		if (profil == null)
 			throw new ObjetNotFoundException(new Profil(), ErrorsEnum.ERR_MCS_PROFIL_0007);
 
-		UserAuth userAuth = userAuthRSM.findByUserName(userId);
+//		UserAuth userAuth = userAuthRSM.findByUserName(userId);
 
-		if (userAuth == null) {
-			throw new ObjetNotFoundException(new Profil(), ErrorsEnum.ERR_MCS_PROFIL_0007);
-		}
+//		if (userAuth == null) {
+//			throw new ObjetNotFoundException(new Profil(), ErrorsEnum.ERR_MCS_PROFIL_0007);
+//		}
 
 		// TODO LIN-440
 		// Generate key validation profile
@@ -695,22 +695,24 @@ public class SignUpSAImpl implements SignUpSA {
 		profil.setExpirationKeyValidateProfil(expirationKeyValidateProfil);
 		profilCUDSM.update(profil);
 
-		Registration registration = null;
+//		Registration registration = null;
 
 		// Create a registration mongo collection
-		registration = registrationFactory.getEntityInstance(userAuth.getUsername(), userAuth.getMail());
-		registration = registrationCUDSM.save(registration);
+//		registration = registrationFactory.getEntityInstance(userAuth.getUsername(), userAuth.getMail());
+//		registration = registrationCUDSM.save(registration);
 
 		// Send Notification mail.
 //		MailParametersDTO mailParametersDTO = mailParametersDTOFactory.getInstance(1, "fr", profil.getEmail(),
 //				registration.getId(), profil.getUsername(), profil.getFirstname());
+//		MailParametersDTO mailParametersDTO = mailParametersDTOFactory.getInstance(1, "fr", profil.getEmail(),
+//				registration.getId(), profil.getKeyValidateProfil().toString(), profil.getFirstname());
 		MailParametersDTO mailParametersDTO = mailParametersDTOFactory.getInstance(1, "fr", profil.getEmail(),
-				registration.getId(), profil.getKeyValidateProfil().toString(), profil.getFirstname());
+		null, profil.getKeyValidateProfil().toString(), profil.getFirstname());
 		notificationMCS.sendEmail(mailParametersDTO);
 
 		// send new Person Physique Action
-		notificationSA.sendNewPPHAction(userAuth.getUsername());
-		isMailSendDTO.setMailSend(true);
+//		notificationSA.sendNewPPHAction(userAuth.getUsername());
+//		isMailSendDTO.setMailSend(true);
 
 		return isMailSendDTO;
 	}
